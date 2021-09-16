fcaa128d3031f21a69fcd866bea3289d9a0bd24b
shelbyreese47 <shelbyreese47@gmail.com>
2021-09-16 15:21:24 -0600

    complete recipe import from API

--- src/components/beerDetails/BeerDetails.jsx
+++ src/components/beerDetails/BeerDetails.jsx
@@ -8,6 +8,11 @@ function BeerDetails({ setId}) {
     const { id } = useParams();
     let mash = random.method.mash_temp;
     let boil=random.boil_volume
+    let fermentation = random.method.fermentation;
+    let malt = random.ingredients.malt;
+    let hops = random.ingredients.hops;
+    let yeast = random.ingredients.yeast;
+    let foodPairing = random.food_pairing;
     // console.log(mash)
     
 
@@ -39,8 +44,38 @@ function BeerDetails({ setId}) {
             <h5>IBU: {random.ibu}</h5>
             <p>{random.description}</p>
             <h4>Recipe</h4>
-            <p>Boil Volume: {boil.value} {boil.unit}</p>
-            <p>Mash at {mash[0].temp.value} {mash[0].temp.unit} for {mash[0].duration} minutes</p>
+            <h5>Ingredients</h5>
+            <p>Malt</p>
+            <ul> 
+                {malt.map((malt)=> 
+                <li>{malt.amount.value} {malt.amount.unit} {malt.name}</li>
+                )}              
+            </ul>
+            <p>Hops</p>
+            <ul> 
+                {hops.map((hops)=> 
+                <li>{hops.amount.value} {hops.amount.unit} {hops.name} to be added at {hops.add} to add {hops.attribute}</li>
+                )}              
+            </ul>
+            <p>Yeast</p>
+            <ul>
+                <li>{yeast}</li>
+            </ul>
+            <h5>Directions:</h5>
+            <ul>
+                <li>Boil Volume: {boil.value} {boil.unit}</li>
+                <li>Mash at {mash[0].temp.value} {mash[0].temp.unit} for {mash[0].duration} minutes</li>
+                <li>Ferment at {fermentation.temp.value} {fermentation.temp.unit}</li>
+            </ul>
+            <p>Brewer's tips:</p>
+                <p>{random.brewers_tips}</p>
+            <p>Food Pairing:</p>
+            <ul>
+                {foodPairing.map((food)=>
+                <li>{food}</li>
+                )}
+            </ul>
+            
         </div>
     );
 }
